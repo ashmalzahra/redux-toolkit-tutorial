083f466951107ed5035f6e40480622dab9d067f7
ashmalzahra <ashmal.zahraa@gmail.com>
2023-02-28 09:06:46 +0500

    Complete project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,36 @@
+import CartContainer from "./components/CartContainer";
+import Navbar from "./components/Navbar";
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { calculateTotals, getCartItems } from './features/cart/cartSlice';
+import Modal from "./components/Modal";
+
 function App() {
-  return <h2>Redux Toolkit</h2>;
+  const { cartItems, isLoading } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+  const { isOpen } = useSelector((state) => state.modal);
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className='loading'>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+
+  return <main>
+    {isOpen && <Modal />}
+    <Navbar />
+    <CartContainer />
+  </main>
 }
 export default App;
